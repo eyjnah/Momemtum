320e88379e3c44ca9e13d50e786b5e764fae2eb7
eyjnah <121935465+eyjnah@users.noreply.github.com>
2023-04-12 03:29:27 +0900

    Add weatherImg

--- js/weather.js
+++ js/weather.js
@@ -4,12 +4,18 @@ function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
-        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const icon = document.querySelector(".weatherImg");
+            const city = document.querySelector("#weather .city");
+            const weather = document.querySelector("#weather .weather");
+            const iconImg = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            icon.setAttribute('src', iconImg)
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main}, ${Math.round(data.main.temp)}°`;
+
+        });
 }
 
 function onGeoError() {
